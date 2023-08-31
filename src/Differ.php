b3b32b32eac565cddf207f105e18600b6bb8b79c
Kengston <danidc@mail.ru>
2023-09-01 01:51:57 +0400

    diff sort of done

--- src/Differ.php
+++ src/Differ.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Differ;
+namespace Differ\Differ;
 
 use function Funct\Collection\union;
 
@@ -8,7 +8,30 @@ function findDiff($pathToFile1, $pathToFile2) {
     $data1 = json_decode(file_get_contents($pathToFile1), true);
     $data2 = json_decode(file_get_contents($pathToFile2), true);
 
-    return union(array_keys($data1), array_keys($data2));
+    $keys = union(array_keys($data1), array_keys($data2));
+
+    $diff = array_map(function ($key) use ($data1, $data2) {
+       if (!array_key_exists($key, $data1)) {
+           return "  + {$key}: " . formatValue($data2[$key]);
+       }
+       if (!array_key_exists($key, $data2)) {
+           return "  - {$key}: " . formatValue($data1[$key]);
+       }
+       if ($data1[$key] === $data2[$key]) {
+           return "    {$key}: " . formatValue($data1[$key]);
+       }
+       return "  - {$key}: " . formatValue($data1[$key]) . PHP_EOL .
+              "  + {$key}: " . formatValue($data2[$key]);
+    }, $keys);
+
+    return '{' . PHP_EOL . implode(PHP_EOL, $diff) . PHP_EOL . '}';
+}
+
+function formatValue($value) {
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    }
+    return is_array($value) ? '[complex value]' : $value;
 }
 
 
--- src/cli.php
+++ src/cli.php
@@ -1,8 +1,33 @@
 <?php
 
+namespace Differ\Cli;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use function Differ\findDiff;
+use function Differ\Differ\findDiff;
 
-$diff = findDiff("file1.json", "file2.json");
-print_r($diff);
+const DOC = <<<DOC
+gendiff -h
+
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
+DOC;
+
+function run()
+{
+    $args = \Docopt::handle(DOC, ['version' => '0.1']);
+
+    $firstFilePath = $args['<firstFile>'];
+    $secondFilePath = $args['<secondFile>'];
+
+    print_r(findDiff($firstFilePath, $secondFilePath));
+}
