88fb6999a7a2cb10fa04c771058c48c25348df3b
Kengston <danidc@mail.ru>
2023-09-06 16:09:45 +0400

    actual version dlya Denis

---
+++ tests/DifferTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Differ\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function Differ\Differ\findDiff;
+
+class GenDiffTest extends TestCase
+{
+    public function testDiffJsonFlat()
+    {
+        $diff = findDiff(__DIR__ . '/files/file1.json', __DIR__ . '/files/file2.json');
+
+        $this->assertStringEqualsFile(__DIR__ . '/files/diff.txt', $diff);
+    }
+}
